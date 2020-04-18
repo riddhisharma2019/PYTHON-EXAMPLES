9e88be9e8a205bac6811d6c1bb65bac5b02eb4d3
riddhisharma2019 <54709862+riddhisharma2019@users.noreply.github.com>
2020-04-18 17:10:59 +0530

    Add files via upload

---
+++ python11.py
@@ -0,0 +1,7 @@
+name='riddhisharma'
+if len(name)<3:
+    print("name must be atleast 3 characters")
+elif len(name)>50:
+    print("name must be atleast max of 50 characters")
+else:
+    print("name looks great!!")        
