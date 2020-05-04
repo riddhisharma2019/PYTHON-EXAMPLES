8ef4bafa3fccca7b70ee861e9d24a7f597974525
riddhisharma2019 <54709862+riddhisharma2019@users.noreply.github.com>
2020-05-04 22:57:46 +0530

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,12 @@
+def greet():                                 # defining the function #
+
+    print("hello riddhi")
+    print("good morning")
+
+
+def add(x,y):
+    c= x+y
+    print(c)
+
+greet()                                               # calling the same function #
+add(5,3)    
