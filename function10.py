5edfafcca31ed3fad6bb5c23a7decf308e16e113
riddhisharma2019 <54709862+riddhisharma2019@users.noreply.github.com>
2020-05-04 23:01:27 +0530

    Add files via upload

---
+++ function10.py
@@ -0,0 +1,9 @@
+def change_string(str):
+     str = str + "hows you"
+     print("printing the string inside function: ",str)
+
+string1 = "hii I am there "     
+
+
+change_string(string1)
+print("printing the string outside function: ",string1)
